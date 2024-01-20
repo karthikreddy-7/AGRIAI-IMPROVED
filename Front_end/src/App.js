5c9082c07730ddd1b8c3704ff2ab458e4a8df350
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-01-20 11:12:00 +0530

    created react project

---
+++ Front_end/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-4xl">
+      This is a component using Tailwind CSS!
+    </div>
+  );
+}
+
+export default App;
