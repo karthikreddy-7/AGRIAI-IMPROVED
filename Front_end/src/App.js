02c0bd8ae4e7e71f8ce6ab13c7e6ed0dcd3a776f
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-01-26 19:19:55 +0530

    added frontend

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -16,9 +16,22 @@ function App() {
             />
             <div className="absolute inset-0 bg-black opacity-30"></div>
           </div>
-          <div className="absolute top-1/3 left-1/4 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
+          <div className="absolute top-1/3 left-1/4 mt-10 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
             <div>AGRICULTURE</div>
             <div>& AI</div>
+          </div>
+
+          <div className=" absolute top-1/2 left-1/4 transform -translate-x-1/2 -translate-y-1/2 text-white text-sm font-bold">
+            <div>A Groundbreaking Initiative That Harnesses The Power</div>
+            <div>
+              {" "}
+              Of Machine Learning To Revolutionize Agriculture Practices.
+            </div>
+
+            <button className="absolute top-1/10 left-1/8 mt-10 font-bold btn rounded-full text-green-800  ">
+              {" "}
+              VIEW SOURCE CODE{" "}
+            </button>
           </div>
           <div className="bg-green-100 rounded-xl shadow-md box-border h-16 w-11/12 absolute top-6 left-12">
             <NavBar />
